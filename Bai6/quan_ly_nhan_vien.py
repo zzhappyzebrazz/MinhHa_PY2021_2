9c836c4552e68cda4650c9811a54629b03682267
zzhappyzebrazz <b8.bbl.lmh@gmail.com>
2022-11-12 11:13:45 +0700

    6.1 6.2

--- Bai6/quan_ly_nhan_vien.py
+++ Bai6/quan_ly_nhan_vien.py
@@ -16,25 +16,25 @@ class NhanVien:
         self.tn_chiu_thue = self.tn_truoc_thue - 11000000 - (self.gia_canh * 4400000)
         
         if self.tn_chiu_thue < 5000000:
-            self.thue_TNCN = self.tn_chiu_thue * 0.05
+            self.thue_TNCN = self.tn_truoc_thue * 0.05
 
         elif self.tn_chiu_thue >= 5000000 and self.tn_chiu_thue < 10000000:
-            self.thue_TNCN = self.tn_chiu_thue * 0.1 - 250000
+            self.thue_TNCN = self.tn_truoc_thue * 0.1 - 250000
         
         elif self.tn_chiu_thue >= 10000000 and self.tn_chiu_thue < 18000000:
-            self.thue_TNCN = self.tn_chiu_thue * 0.15 - 750000
+            self.thue_TNCN = self.tn_truoc_thue * 0.15 - 750000
             
         elif self.tn_chiu_thue >= 18000000 and self.tn_chiu_thue < 32000000:
-            self.thue_TNCN = self.tn_chiu_thue * 0.20 - 1650000
+            self.thue_TNCN = self.tn_truoc_thue * 0.20 - 1650000
 
         elif self.tn_chiu_thue >= 32000000 and self.tn_chiu_thue < 52000000:
-            self.thue_TNCN = self.tn_chiu_thue * 0.25 - 3250000
+            self.thue_TNCN = self.tn_truoc_thue * 0.25 - 3250000
             
         elif self.tn_chiu_thue >= 52000000 and self.tn_chiu_thue < 80000000:
-            self.thue_TNCN = self.tn_chiu_thue * 0.30 - 5850000
+            self.thue_TNCN = self.tn_truoc_thue * 0.30 - 5850000
         
         else:
-            self.thue_TNCN = self.tn_chiu_thue * 0.35 - 9850000
+            self.thue_TNCN = self.tn_truoc_thue * 0.35 - 9850000
 
         self.tn_sau_thue = self.tn_truoc_thue - self.thue_TNCN
         
@@ -72,19 +72,26 @@ class QuanLyNhanVien:
 
     def NhapNhanVien(self):
         while(1):
-            ho_ten = input("Nhập họ và tên nhân viên: ")
-            he_so_luong = float(input("Nhập hệ số lương: "))
-            giam_tru_gia_canh = int(input("Nhập số người giảm trừ gia cảnh: "))
-            thu_lao = float(input("Nhập thù lao công việc: "))
-            nv = NhanVien(ho_ten, he_so_luong, giam_tru_gia_canh, thu_lao)
-            self.__danh_sach_NV.append(nv)
-            if not int(input('Tiếp tục nhập nhân viên? 0 no : 1 yes')):
+            chon = int(input("Chọn kiểu nhân viên: 1 Nhân viên sản xuất; 2 Nhân viên kinh doanh; 0 Hủy: "))              
+            if not chon:
                 break
+            else:
+                ho_ten = input("Nhập họ và tên nhân viên: ")
+                he_so_luong = float(input("Nhập hệ số lương: "))
+                giam_tru_gia_canh = int(input("Nhập số người giảm trừ gia cảnh: "))
+                if chon == 1:
+                    doanh_so = int(input("Nhập danh số cho nhân viên kinh doanh: "))
+                    nv = NhanVienKinhDoanh(ho_ten, he_so_luong, giam_tru_gia_canh, doanh_so)
+                elif chon == 2:
+                    san_pham = int(input("Nhập số sản phẩm cho nhân viên sản xuất: "))
+                    nv = NhanVienSanXuat(ho_ten, he_so_luong, giam_tru_gia_canh, san_pham)
+                self.__danh_sach_NV.append(nv)
 
     def ThongTinNhanVien(self):
         print(f'Số nhân viên đang được quản lý: {len(self.__danh_sach_NV)}')
         for i in range(len(self.__danh_sach_NV)):
-            print(f'Nhân viên {i + 1}: {self.__danh_sach_NV[i].ho_ten}')
+            print(self.__danh_sach_NV[i].loaiNhanVien())
+            print(f'Nhân viên {i + 1}: {self.__danh_sach_NV[i]}')
 
     def __del__(slef):
         print('dtor QuanLyNhanVien')
@@ -92,7 +99,25 @@ class QuanLyNhanVien:
     def __str__(self):
         for nv in self.__danh_sach_NV:
             print(nv)
-      
+
+class NhanVienKinhDoanh(NhanVien):
+    def __init__(self, ho_ten, he_so_luong, gia_canh, ty_le_kinh_doanh):
+        self.luong_cb = 1600000
+        super().__init__(ho_ten, he_so_luong, gia_canh, ty_le_kinh_doanh * self.luong_cb)
+    
+    def loaiNhanVien(self):
+        return 'Nhân viên kinh doanh'
+
+class NhanVienSanXuat(NhanVien):
+    def __init__(self, ho_ten, he_so_luong, gia_canh, so_san_pham):
+        self.don_gia_gia_cong = 15000
+        self.dinh_muc_san_pham = 450
+        if so_san_pham > self.dinh_muc_san_pham:
+            super().__init__(ho_ten, he_so_luong, gia_canh, (so_san_pham - self.dinh_muc_san_pham) * self.don_gia_gia_cong)
+
+    def loaiNhanVien(self):
+        return 'Nhân viên Sản xuất'
+
 
 def main():
     # ho_ten = input("Nhập họ và tên nhân viên: ")
@@ -108,7 +133,7 @@ def main():
     mgr = QuanLyNhanVien()
     mgr.NhapNhanVien()
     mgr.ThongTinNhanVien()
-    mgr.__str__()
+    # mgr.__str__()
     # print(nv)
     
 if __name__ == "__main__":
