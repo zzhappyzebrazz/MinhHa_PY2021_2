aa95064457f7d8b31b12df5dac8ee5d6c70b44c9
zzhappyzebrazz <b8.bbl.lmh@gmail.com>
2022-11-12 12:30:21 +0700

    6.3 6.4

--- Bai6/MyNews/stories/admin.py
+++ Bai6/MyNews/stories/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Catergory, Story, Contact
 # Register your models here.
+
+admin.site.register(Catergory)
+admin.site.register(Story)
+admin.site.register(Contact)
