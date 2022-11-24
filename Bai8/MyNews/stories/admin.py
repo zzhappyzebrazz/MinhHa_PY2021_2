b1ef6e14fbb35121b539a7343642118f3fa6e96d
zzhappyzebrazz <b8.bbl.lmh@gmail.com>
2022-11-24 20:50:53 +0700

    enable ckeditor in /admin/ page

---
+++ Bai8/MyNews/stories/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Category, Story, Contact
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Story)
+admin.site.register(Contact)
---
+++ Bai8/MyNews/stories/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils import timezone
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Story(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    name = models.CharField(max_length=150, unique=True)
+    author = models.CharField(max_length=250)
+    url = models.URLField(null=True)
+    summary = models.TextField(null=True, blank=True)
+    content = RichTextUploadingField(blank=True, null=True)
+    public_day = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='stories/images', default='stories/images/logo.jpg')
+    viewed = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
+    
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    subject = models.CharField(max_length=300)
+    message = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
